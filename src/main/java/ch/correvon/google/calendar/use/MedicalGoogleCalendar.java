097bdda0f0c85e5c613eeb2f3bceb21ee5527ef0
loic-pryv <loic@pryv.com>
2021-01-07 14:29:38 +0100

    rename

--- src/main/java/ch/correvon/google/calendar/use/MedicalGoogleCalendar.java
+++ src/main/java/ch/correvon/google/calendar/use/MedicalGoogleCalendar.java
@@ -6,20 +6,21 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import ch.correvon.google.calendar.window.mainWindow.MainWindow;
 
-public class MainGoogleCalendar
+public class MedicalGoogleCalendar
 {
+	private static Log s_logger = LogFactory.getLog(MedicalGoogleCalendar.class);
+	public static final String APPLICATION_NAME = "PUMBAA google cal Quick Entry (Medical Google Calendar)";
+	private static final String VERSION = "1.0.0";
+
 	/*
 	 * https://console.developers.google.com/apis/credentials?project=medical-calendar-1125
 	 */
 	public static void main(String[] args) throws ParseException, InterruptedException
 	{
 		s_logger.info("Démarrage du programme");
-		new MainWindow("Medical Google Calendar (" + VERSION + ")").run();
+		new MainWindow(APPLICATION_NAME + " (" + VERSION + ")").run();
 	}
 
-	private static Log s_logger = LogFactory.getLog(MainGoogleCalendar.class);
-	private static final String VERSION = "0.4";
-	
 	/*
 	 * TODO
 	 * ne pas mettre le jour (de la date du jour) en surbrillance si on n'est pas sur le bon mois 
